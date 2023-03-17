1686ac70dbe19d18a764c6843f1d4e63ae275aad
Hyeong1204 <cho4870@naver.com>
2023-03-17 11:15:01 +0900

    아이템 데이터틀 완성

---
+++ Hungry Blacksmith/Assets/Script/ScriptableObject/ItemData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Data", menuName = "ScriptableObject/ItemData", order = 0)]
+public class ItemData : ScriptableObject
+{
+    [Header("아이템 기본 데이터")]
+    public int itemID;
+    public string itemName;
+    public GameObject itemPrefab;
+    public Sprite itemIcon;
+    [Header("아이템 구매 비용")]
+    public int buyValue;
+    [Header("아이템 판매 비율")]
+    [Range(0f, 1f)]
+    public float saleValue;
+    public int SaleValue
+    {
+        get => (int)(buyValue * saleValue);       // 무조건소수점 잘라냄
+    }
+
+}
