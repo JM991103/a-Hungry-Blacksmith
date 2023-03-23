594b769bc58565f65b7f4d9b32aad21856dac8d4
JM991103 <ljn1103@naver.com>
2023-03-23 10:15:15 +0900

    UI 골드 및 날짜 추가

--- Hungry Blacksmith/Assets/Script/Core/GameManager.cs
+++ Hungry Blacksmith/Assets/Script/Core/GameManager.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    
+    UI ui;
+
+    public UI UI => ui;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        ui = FindObjectOfType<UI>();
+    }
 }
--- Hungry Blacksmith/Assets/Script/ScenetoButton/mainScenebutton.cs
+++ Hungry Blacksmith/Assets/Script/ScenetoButton/mainScenebutton.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,18 +9,33 @@ using UnityEngine.UI;
 public class mainScenebutton : MonoBehaviour
 {
     public string loadScene;
+    public int hpDown = 0;
 
     Button mainSceneButton;
+    TextMeshProUGUI mainSceneText;
 
     private void Awake()
     {
         mainSceneButton = GetComponent<Button>();
-        mainSceneButton.onClick.AddListener(ShopButton);
+        mainSceneButton.onClick.AddListener(ChangeButton);
+        mainSceneText = mainSceneButton.GetComponentInChildren<TextMeshProUGUI>();
     }
 
-    void ShopButton()
+    void ChangeButton()
     {
-        SceneManager.LoadScene(loadScene);
+        if ((GameManager.Inst.UI.HP - hpDown) > -1)
+        {
+            SceneManager.LoadScene(loadScene);
+            GameManager.Inst.UI.HP -= hpDown;
+            Debug.Log($"HP가{hpDown}만큼 감소되었습니다.  현재 HP:{GameManager.Inst.UI.HP}");
+            if (SceneManager.GetActiveScene().name == "main")
+            {
+                GameManager.Inst.UI.DayTextParent.gameObject.SetActive(false);
+            }
+            else
+            {
+                GameManager.Inst.UI.DayTextParent.gameObject.SetActive(true);
+            }
+        }
     }
-
 }
