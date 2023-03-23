f7848aa6e9ef8a6bd378b15e474cb14ee540d079
Hyeong1204 <cho4870@naver.com>
2023-03-23 14:18:05 +0900

    형욱이 큰일났어

--- Hungry Blacksmith/Assets/Script/Core/GameManager.cs
+++ Hungry Blacksmith/Assets/Script/Core/GameManager.cs
@@ -7,13 +7,25 @@ public class GameManager : Singleton<GameManager>
     UI ui;
     WeaponItemManager weaponManager;
 
+    Weapon[] weapons;
+
     public UI UI => ui;
     public WeaponItemManager WeaponManager => weaponManager;
+    public Weapon[] Weapons => weapons;
 
     protected override void Initialize()
     {
         base.Initialize();
         ui = FindObjectOfType<UI>();
         weaponManager = GetComponent<WeaponItemManager>();
+        weapons = new Weapon[3];
+    }
+
+    public void WeaponSave(Weapon waepon, int count)
+    {
+        if(weapons != null)
+        {
+            weapons[count] = waepon;
+        }
     }
 }
