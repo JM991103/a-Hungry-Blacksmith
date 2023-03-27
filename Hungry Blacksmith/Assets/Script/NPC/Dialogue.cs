b88d4345828b433837de24785faed77d73c1c74c
JM991103 <ljn1103@naver.com>
2023-03-27 14:33:05 +0900

    다이얼로그 추가

---
+++ Hungry Blacksmith/Assets/Script/NPC/Dialogue.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueText
+{
+    [TextArea]
+    public string dialogue;
+}
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField]
+    private DialogueText[] dialogueTexts;
+
+    TextMeshProUGUI text;
+
+    int count = 0;
+
+    public void ShowDialogue()
+    {
+        ShowOnOff(true);
+
+        count = 0;
+    }
+
+    public void ShowOnOff(bool flag)
+    {
+        gameObject.SetActive(flag);
+        text.gameObject.SetActive(flag);
+    }
+
+    public void NextDialogue()
+    {
+        text.text = dialogueTexts[count].dialogue;
+        count++;
+    }
+
+    private void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        
+    }
+    private void Start()
+    {
+        ShowOnOff(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (count < dialogueTexts.Length)
+            {
+                NextDialogue();
+            }
+            else
+            {
+                ShowOnOff(false);
+            }
+        }
+    }
+
+}
